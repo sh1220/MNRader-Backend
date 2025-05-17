6d19c9120578123ebe044e5afe76c529b687b22f
sh1220 <jiu0009@naver.com>
2025-05-17 16:22:45 +0900

    feat: first init

---
+++ src/main/java/com/example/mnraderbackend/MnRaderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mnraderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnRaderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MnRaderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mnraderbackend/MnRaderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mnraderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnRaderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
